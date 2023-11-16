e24937247400d92d4a0b9c936e8a2cae31805490
James Radtke <cloudxabide@gmail.com>
2023-11-15 21:16:38 -0500

    Updated to ISC-DHCP

---
+++ Scripts/Install_DHCP_Server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NEEDRESTART_MODE=a
+
+sudo apt install -y isc-dhcp-server
+
+
+cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.orig
+curl https://raw.githubusercontent.com/cloudxabide/eksa.matrix.lab/main/Files/etc_dhcp_dhcpd.conf | sudo tee /etc/dhcp/dhcpd.conf
+curl https://raw.githubusercontent.com/cloudxabide/eksa.matrix.lab/main/Files/etc_dhcp_dhcpd-hosts.conf | sudo tee /etc/dhcp/dhcpd-hosts.conf
+systemctl restart isc-dhcp-server.service
+
+
+journalctl -f -u isc-dhcp-server.service
