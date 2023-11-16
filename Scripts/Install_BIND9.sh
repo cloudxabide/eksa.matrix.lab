0705d84d227fef2415f4bad1594a426262985eb8
James Radtke <cloudxabide@gmail.com>
2023-11-15 20:09:07 -0500

    New Zone Files

--- Scripts/Install_BIND9.sh
+++ Scripts/Install_BIND9.sh
@@ -6,7 +6,8 @@ sudo apt install -y bind9 bind9utils bind9-doc
 sudo sed -i -e 's/bind/bind -4/g' /etc/default/named
 
 sudo cp /etc/bind/named.conf.options /etc/bind/named.conf.options.bak
-
+curl https://raw.githubusercontent.com/cloudxabide/eksa.matrix.lab/main/Files/etc_bind_named.conf.options | sudo tee /etc/bind/named.conf.options
+sudo systemctl restart bind9  # You now have a caching nameserver using Google DNS as forwarders
  
 sudo mkdir -p /chroot/named
 cd /chroot/named
@@ -23,5 +24,7 @@ named-checkzone eksa.matrix.lab /etc/bind/db.eksa.matrix.lab
 
 
 references() {
-echo "https://help.ubuntu.com/community/BIND9ServerHowto"
+echo "https://www.digitalocean.com/community/tutorials/how-to-configure-bind-as-a-private-network-dns-server-on-ubuntu-22-04"
+
+echo "Who knows how old this is?  https://help.ubuntu.com/community/BIND9ServerHowto"
 }
