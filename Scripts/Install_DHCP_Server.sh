ee0b441e2d31b67e049c64737d88363648b56b07
James Radtke <cloudxabide@gmail.com>
2023-11-15 21:20:20 -0500

    generic update

--- Scripts/Install_DHCP_Server.sh
+++ Scripts/Install_DHCP_Server.sh
@@ -4,7 +4,6 @@ NEEDRESTART_MODE=a
 
 sudo apt install -y isc-dhcp-server
 
-
 cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.orig
 curl https://raw.githubusercontent.com/cloudxabide/eksa.matrix.lab/main/Files/etc_dhcp_dhcpd.conf | sudo tee /etc/dhcp/dhcpd.conf
 curl https://raw.githubusercontent.com/cloudxabide/eksa.matrix.lab/main/Files/etc_dhcp_dhcpd-hosts.conf | sudo tee /etc/dhcp/dhcpd-hosts.conf
